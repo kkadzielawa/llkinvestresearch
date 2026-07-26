40d6e7d5ae795f4d0ff89f05e4f871baf6a95482
Konrad Kadzielawa <kkadzi25@gmail.com>
2026-07-25 21:18:03 -0500

    seo changes;

--- llkinvestresearch/urls.py
+++ llkinvestresearch/urls.py
@@ -1,15 +1,37 @@
 """llkinvestresearch URL Configuration."""
 
+from django.contrib.sitemaps.views import sitemap
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 from blog import views as blog_views
 
+from .sitemaps import (
+    ArchiveSitemap,
+    ArticleSitemap,
+    ContactSitemap,
+    HomeSitemap,
+    MarketAnalyzerSitemap,
+    MicroViewSitemap,
+    OptionsStudySitemap,
+)
 from .views import ContactView, HomeView, MarketAnalyzerPageView
 
+sitemaps = {
+    "home": HomeSitemap,
+    "archives": ArchiveSitemap,
+    "blog_posts": ArticleSitemap,
+    "microview": MicroViewSitemap,
+    "options": OptionsStudySitemap,
+    "market_analyzer": MarketAnalyzerSitemap,
+    "contact": ContactSitemap,
+}
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), name="robots_txt"),
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap_xml"),
     path("", HomeView.as_view(), name="home"),
     path("microview/", blog_views.MicroViewList.as_view(), name="microview_blog"),
     path(
